74fb91dd68e318a1cb082f3ba620e4ef800c97ec
ModderCodder <1modder.path1@gmail.com>
2025-07-10 22:42:43 +0300

    Initial Commit

---
+++ BinarySum/BinarySumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinarySumApp.swift
+//  BinarySum
+//
+//  Created by Moddeeeeeeep on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BinarySumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
